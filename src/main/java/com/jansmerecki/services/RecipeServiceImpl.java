3ed56191d52bd3b4b63c29d73d1d67a326175a9a
JSmerec98 <jasiek.smerecki@gmail.com>
2020-08-16 16:38:31 +0200

    Refactored to Project Lombok

--- src/main/java/com/jansmerecki/services/RecipeServiceImpl.java
+++ src/main/java/com/jansmerecki/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.jansmerecki.services;
 
 import com.jansmerecki.domain.Recipe;
 import com.jansmerecki.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -16,12 +18,10 @@ public class RecipeServiceImpl implements RecipeService {
         this.recipeRepository = recipeRepository;
     }
 
-
     @Override
     public Set<Recipe> getRecipes() {
-
+        log.debug("I'm in the service!");
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
