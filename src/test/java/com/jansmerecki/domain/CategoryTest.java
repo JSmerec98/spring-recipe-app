5507d9425f07882cfb8939361b6a74e7ec0ab1a4
JSmerec98 <jasiek.smerecki@gmail.com>
2020-08-18 19:52:22 +0200

    Added JUnit test for Category.

---
+++ src/test/java/com/jansmerecki/domain/CategoryTest.java
@@ -0,0 +1,32 @@
+package com.jansmerecki.domain;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @org.junit.Test
+    public void getId() {
+
+        Long idValue = 4L;
+        category.setId(4L);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @org.junit.Test
+    public void getDescription() {
+    }
+
+    @org.junit.Test
+    public void getRecipes() {
+    }
+}
