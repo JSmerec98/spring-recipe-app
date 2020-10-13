412b4602565585d18e6462014950e9ba704d2aeb
JSmerec98 <jasiek.smerecki@gmail.com>
2020-10-13 12:54:00 +0200

    Added Delete for one ingredient.

--- src/main/java/com/jansmerecki/services/IngredientService.java
+++ src/main/java/com/jansmerecki/services/IngredientService.java
@@ -4,5 +4,8 @@ import com.jansmerecki.commands.IngredientCommand;
 
 public interface IngredientService {
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
